49d9583ffbdc1590fab7f191079dc25aa8f59b3b
Jinger <lhj69382@naver.com>
2023-01-29 10:47:43 +0900

    alter repository 012911

--- src/main/java/com/bicycle/project/oauthlogin/repository/RefreshTokenJpaRepo.java
+++ src/main/java/com/bicycle/project/oauthlogin/repository/RefreshTokenJpaRepo.java
@@ -3,9 +3,11 @@ package com.bicycle.project.oauthlogin.repository;
 import com.bicycle.project.oauthlogin.data.entity.UserRefreshToken;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenJpaRepo extends JpaRepository<UserRefreshToken, String> {
 
     Optional<UserRefreshToken> findByKey(Long key);
